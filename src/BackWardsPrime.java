f3d9b445fd04bcfbd18bf041d4d830bd0e70074d
irezwi <zwirek.ireneusz@gmail.com>
2018-02-05 00:39:14 +0100

    Backwards Read Primes completed

--- src/BackWardsPrime.java
+++ src/BackWardsPrime.java
@@ -1,8 +1,13 @@
-public class BackWardsPrime {
-    public static String backwardsPrime(long start, long end) {
-
-
-        return "a";
+class BackWardsPrime {
+    private static String backwardsPrime(long start, long end) {
+        StringBuilder result = new StringBuilder();
+        for(long i = start; i <= end; i++) {
+            if(isBackwardsPrime(i))
+                result.append(i).append(" ");
+        }
+        if(result.length() > 2)
+            result.deleteCharAt(result.length()-1);
+        return result.toString();
     }
 
     static boolean isPrime(long number) {
@@ -13,7 +18,17 @@ public class BackWardsPrime {
         return true;
     }
 
-    public static void main(String[] args) {
+    static boolean isBackwardsPrime(long number) {
+        return number >= 10 && isPrime(number) && isPrime(getReverseInt(number)) && (number != getReverseInt(number));
+    }
 
+    static long getReverseInt(long number) {
+        if(number < 10)
+            return number;
+        long resultNumber = 0;
+        for (long i = number; i !=0; i /= 10) {
+            resultNumber = resultNumber * 10 + i % 10;
+        }
+        return resultNumber;
     }
 }
--- src/BackWardsPrimeTest.java
+++ src/BackWardsPrimeTest.java
@@ -5,12 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class BackWardsPrimeTest {
     @Test
     void isPrimeTest1() {
-        assertEquals(false, BackWardsPrime.isPrime(34));
+        assertEquals(true, BackWardsPrime.isPrime(199));
     }
 
     @Test
     void isPrimeTest2() {
-        assertEquals(true, BackWardsPrime.isPrime(31));
+        assertEquals(true, BackWardsPrime.isPrime(991));
     }
 
+    @Test
+    void isBackwardsPrimeTest() {
+        assertEquals(true, BackWardsPrime.isBackwardsPrime(199));
+    }
+
+    @Test
+    void reverseIntTest() {
+        assertEquals(991, BackWardsPrime.getReverseInt(199));
+    }
 }
