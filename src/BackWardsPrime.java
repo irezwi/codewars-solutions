64032d78626f116a99676c306c931040ea9b5872
irezwi <zwirek.ireneusz@gmail.com>
2018-02-06 18:02:15 +0100

    Reformatted code

--- src/BackWardsPrime.java
+++ src/BackWardsPrime.java
@@ -1,18 +1,18 @@
 class BackWardsPrime {
     private static String backwardsPrime(long start, long end) {
         StringBuilder result = new StringBuilder();
-        for(long i = start; i <= end; i++) {
-            if(isBackwardsPrime(i))
+        for (long i = start; i <= end; i++) {
+            if (isBackwardsPrime(i))
                 result.append(i).append(" ");
         }
-        if(result.length() > 2)
-            result.deleteCharAt(result.length()-1);
+        if (result.length() > 2)
+            result.deleteCharAt(result.length() - 1);
         return result.toString();
     }
 
     static boolean isPrime(long number) {
-        for(int i = 2; i < number; i++) {
-            if(number%i == 0)
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0)
                 return false;
         }
         return true;
@@ -23,10 +23,10 @@ class BackWardsPrime {
     }
 
     static long getReverseInt(long number) {
-        if(number < 10)
+        if (number < 10)
             return number;
         long resultNumber = 0;
-        for (long i = number; i !=0; i /= 10) {
+        for (long i = number; i != 0; i /= 10) {
             resultNumber = resultNumber * 10 + i % 10;
         }
         return resultNumber;
